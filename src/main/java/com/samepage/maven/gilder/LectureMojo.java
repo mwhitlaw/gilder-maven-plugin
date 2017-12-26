d4ebb27bae7f825c65e997237251770b556259f8
Mike Whitlaw <mike.whitlaw@gmail.com>
2017-12-26 16:08:44 -0500

    Trimmed dependencies

--- src/main/java/com/samepage/maven/gilder/LectureMojo.java
+++ src/main/java/com/samepage/maven/gilder/LectureMojo.java
@@ -1,38 +1,28 @@
 package com.samepage.maven.gilder;
 
+import com.samepage.maven.PomManager;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Mojo(name = "lecture", aggregator = true)
 public class LectureMojo extends AbstractMojo {
 
-    @Parameter(defaultValue = "${project}", required = true, readonly = true)
-    private MavenProject rootProject;
-
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-
-
-
-        List moduleNames = rootProject.getModules();
-        getLog().info(String.format("%s: One does not simply add a dependency", rootProject.getName()));
-
-
-
-        if (moduleNames != null && moduleNames.size() > 0) {
-            moduleNames.forEach((i) -> {
-                String moduleName = (String) i;
-                getLog().info(String.format("Module: %s", moduleName));
-            });
-        } else {
-            getLog().info("This project has no modules");
+        try {
+            getLog().info("One does not simply add a dependency");
+            PomManager pomManager = new PomManager();
+            pomManager.printOriginal();
+            pomManager.printWorking();
+        } catch (XmlPullParserException | IOException | URISyntaxException e) {
+            getLog().error(e);
         }
     }
 }
